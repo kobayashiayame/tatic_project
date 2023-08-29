d9ba1fdbe0e7388a6a0e63f241fc4057fded0e24
kobayashiayame <ki1_77@icloud.com>
2023-08-30 03:22:47 +0900

    first commit

---
+++ src/main/java/static_project/Sample.java
@@ -0,0 +1,11 @@
+package static_project;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Sampleです");
+		System.out.println("Java");
+	}
+
+}
