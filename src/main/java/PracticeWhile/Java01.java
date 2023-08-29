c9f9eca3f1219bfbec8be3dcf06c1863ceb66bd4
kobayashiayame <ki1_77@icloud.com>
2023-08-30 05:03:02 +0900

    Java01

---
+++ src/main/java/PracticeWhile/Java01.java
@@ -0,0 +1,15 @@
+package PracticeWhile;
+
+public class Java01 {
+	public class CountDown{
+		
+		public static void main(String[] args) {
+			int num = 100;
+			
+		    while (num >= 1) {
+			   num--;
+			   System.out.println(num);
+		    }
+		}
+	}
+}
